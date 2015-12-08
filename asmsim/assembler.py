d80f9bc405b799c4f041fadfe4c91bba51e8d5a5
joeylmaalouf <joeylmaalouf@gmail.com>
2015-12-07 19:10:47 -0500

    moved syscall to utils

--- asmsim/assembler.py
+++ asmsim/assembler.py
@@ -1,7 +1,7 @@
 from instruction import Instruction
 from preprocessor import label_positions, normalize, preprocess
 from registers import Registers
-from utils import getval
+from utils import getval, syscall
 
 
 class Assembler(object):
@@ -102,99 +102,22 @@ class Assembler(object):
       elif instr.operation in ["slti", "sltiu"]:
         if self.registers[instr.operand1] < getval(instr.operand2, False):
           self.registers[instr.operand0] = 1
-     elif instr.operation == "sll":
+      elif instr.operation == "sll":
         self.registers[instr.operand0] = self.registers[instr.operand1] << getval(instr.operand2, False)
-     elif instr.operation == "sllv":
+      elif instr.operation == "sllv":
         self.registers[instr.operand0] = self.registers[instr.operand1] << self.registers[instr.operand2]
-     elif instr.operation == "sra":
+      elif instr.operation == "sra":
         self.registers[instr.operand0] = self.registers[instr.operand1] >> getval(instr.operand2, True)
-     elif instr.operation == "srl":
+      elif instr.operation == "srl":
         self.registers[instr.operand0] = self.registers[instr.operand1] >> getval(instr.operand2, False)
-     elif instr.operation == "srlv":
+      elif instr.operation == "srlv":
         self.registers[instr.operand0] = self.registers[instr.operand1] >> self.registers[instr.operand2]
       elif instr.operation in ["sub", "subu"]:
         self.registers[instr.operand0] = self.registers[instr.operand1] - self.registers[instr.operand2]
       elif instr.operation == "sw":
         pass # TODO
       elif instr.operation == "syscall":
-        if v0 == 1:
-          print a0
-        elif v0 in [2, 3]:
-        print fl2
-    elif v0 == 4:
-        print a0
-    elif v0 == 5:
-          pass
-    elif v0 == 6:
-        pass
-    elif v0 == 7:
-        pass
-    elif v0 == 8:
-        pass
-    elif v0 == 9:
-        pass
-    elif v0 == 10:
-        pass
-    elif v0 == 11:
-        pass
-    elif v0 == 12:
-        pass
-    elif v0 == 13:
-        pass
-    elif v0 == 14:
-        pass
-    elif v0 == 15:
-        pass
-    elif v0 == 16:
-        pass
-    elif v0 == 17:
-        pass
-    elif v0 == 30:
-        pass
-    elif v0 == 31:
-        pass
-    elif v0 == 32:
-        pass
-    elif v0 == 33:
-        pass
-    elif v0 == 34:
-        pass
-    elif v0 == 35:
-        pass
-    elif v0 == 36:
-        pass
-    elif v0 in [37, 38, 39, 45, 46, 47, 48, 49]:
-        pass
-    elif v0 == 40:
-        pass
-    elif v0 == 41:
-        pass
-    elif v0 == 42:
-    pass
-    elif v0 == 43:
-        pass
-    elif v0 == 44:
-        pass
-    elif v0 == 50:
-        pass
-    elif v0 == 51:
-        pass
-    elif v0 == 52:
-        pass
-    elif v0 == 53:
-        pass
-    elif v0 == 54:
-        pass
-  elif v0 == 55:
-        pass
-    elif v0 == 56:
-        pass
-    elif v0 == 57:
-        pass
-    elif v0 == 58:
-        pass
-    elif v0 == 59:
-        pass
+        syscall(self.registers[2])
       elif instr.operation == "xor":
         self.registers[instr.operand0] = self.registers[instr.operand1] ^ self.registers[instr.operand2]
       elif instr.operation == "xori":
--- asmsim/utils.py
+++ asmsim/utils.py
@@ -11,6 +11,87 @@ def twoscomp(hexstring):
   return val
 
 
+def syscall(v0):
+  if v0 == 1:
+    print a0
+  elif v0 in [2, 3]:
+    print fl2
+  elif v0 == 4:
+    print a0
+  elif v0 == 5:
+    pass
+  elif v0 == 6:
+    pass
+  elif v0 == 7:
+    pass
+  elif v0 == 8:
+    pass
+  elif v0 == 9:
+    pass
+  elif v0 == 10:
+    pass
+  elif v0 == 11:
+    pass
+  elif v0 == 12:
+    pass
+  elif v0 == 13:
+    pass
+  elif v0 == 14:
+    pass
+  elif v0 == 15:
+    pass
+  elif v0 == 16:
+    pass
+  elif v0 == 17:
+    pass
+  elif v0 == 30:
+    pass
+  elif v0 == 31:
+    pass
+  elif v0 == 32:
+    pass
+  elif v0 == 33:
+    pass
+  elif v0 == 34:
+    pass
+  elif v0 == 35:
+    pass
+  elif v0 == 36:
+    pass
+  elif v0 in [37, 38, 39, 45, 46, 47, 48, 49]:
+    pass
+  elif v0 == 40:
+    pass
+  elif v0 == 41:
+    pass
+  elif v0 == 42:
+    pass
+  elif v0 == 43:
+    pass
+  elif v0 == 44:
+    pass
+  elif v0 == 50:
+    pass
+  elif v0 == 51:
+    pass
+  elif v0 == 52:
+    pass
+  elif v0 == 53:
+    pass
+  elif v0 == 54:
+    pass
+  elif v0 == 55:
+    pass
+  elif v0 == 56:
+    pass
+  elif v0 == 57:
+    pass
+  elif v0 == 58:
+    pass
+  elif v0 == 59:
+    pass
+
+
 if __name__ == "__main__":
   print(getval("00000000", True))
   print(getval("7FFFFFFF", True))
