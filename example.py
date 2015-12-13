2047d1162f13f801f16789f39e26a2e9f29cb851
joeylmaalouf <joeylmaalouf@gmail.com>
2015-12-13 15:45:15 -0500

    updated example

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 if __name__ == "__main__":
   from asmsim.assembler import Assembler
-  a = Assembler(open("example.asm", "r"), "MIPS").run()
-  # print(a.registers["$t0"], a.registers["$t1"], a.registers["$t2"])
+  a = Assembler(open("example.asm", "r"), "MIPS")
   offset = a.memory.labels["arr"]
+  print(a.memory[offset], a.memory[offset+1], a.memory[offset+2], a.memory[offset+3])
+  a.run()
   print(a.memory[offset], a.memory[offset+1], a.memory[offset+2], a.memory[offset+3])
