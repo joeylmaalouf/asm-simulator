49061ac0bbdac5fe8d861a5cb800fc0321bb8287
buttegab <gabriel.butterick@students.olin.edu>
2015-12-14 19:16:07 -0500

    thanks joey

--- asmsim/flags.py
+++ asmsim/flags.py
@@ -8,39 +8,19 @@ class Flags(object):
     self.Zflag = words[1] if len(words) > 1 else None
     self.Cflag = words[2] if len(words) > 2 else None
     self.Vflag = words[3] if len(words) > 3 else None
-    
 
-  # def update(self, Nflag, Zflag, Cflag, Vflag):
-  #   """ Update the flags attributes all at once. """
-  #   if Nflag != None: self.Nflag = Nflag
-  #   if Zflag != None: self.Zflag = Zflag
-  #   if Cflag != None: self.Cflag = Cflag
-  #   if Vflag != None: self.Vflag = Vflag
-  
-  def update(self,*flag):
-    """ Update the flags attributes all at once. """
-    if flag[0] != None: self.Nflag = flag[0]
-    if len(flag) > 1 and flag[1] != None: self.Zflag = flag[1] 
-    if len(flag) > 2 and flag[2] != None: self.Cflag = flag[2] 
-    if len(flag) > 3 and flag[3] != None: self.Vflag = flag[3] 
-
-
-  def updateN(self, Nflag):
-  	if Nflag != None: self.Nflag = Nflag
-
-  def updateZ(self, Zflag):
-  	if Zflag != None: self.Zflag = Zflag
-
-  def updateC(self, Cflag):
-  	if Cflag != None: self.Cflag = Cflag
-
-  def updateV(self, Vflag):
-  	if Vflag != None: self.Vflag = Vflag
-  
+  #def update(self,*flag):
+  def update(self, N = None, Z = None, C = None, V = None):
+    """ Update the flags attributes all at once. Format is N/Z/V/C = (a number) """
+    if N != None: self.Nflag = N
+    if Z != None: self.Zflag = Z 
+    if C != None: self.Cflag = C 
+    if V != None: self.Vflag = V 
 
 if __name__ == "__main__":
   flags = Flags("0, 0, 0, 0")
   print(flags.raw)
   print(flags.Nflag, flags.Zflag, flags.Cflag, flags.Vflag)
-  flags.update(1, , 1, None)
+  flags.update(1, None, 1, None)
+  flags.update(N = 1, C = 1)
   print(flags.Nflag, flags.Zflag, flags.Cflag, flags.Vflag)
